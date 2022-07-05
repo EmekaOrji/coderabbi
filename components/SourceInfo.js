cfbe55570dd0abd23003b7c3eafe230954e55393
Emeka Orji <emekapraiseo@gmail.com>
2022-07-05 19:33:45 +0100

    chore: darkmode

---
+++ components/SourceInfo.js
@@ -0,0 +1,101 @@
+const CodeGrepper = {
+	Image: () => (
+		<img src='/result-sources-icons/codegrepper.jpeg' alt='CodeGrepper' />
+	),
+	color: '#72bf3b',
+};
+const Codepen = {
+	Image: () => <img src='/result-sources-icons/codepen.svg' alt='Codepen' />,
+	color: '#0EBEFF',
+};
+const CodeProject = {
+	Image: () => (
+		<img src='/result-sources-icons/codeproject.png' alt='CodeProject' />
+	),
+	color: '#F5D949',
+};
+const CssTricks = {
+	Image: () => (
+		<img src='/result-sources-icons/csstricks.png' alt='CssTricks' />
+	),
+	color: '#C19568',
+};
+const DevTo = {
+	Image: () => <img src='/result-sources-icons/devto.svg' alt='DevTo' />,
+	color: '#29292E',
+};
+const EggheadIo = {
+	Image: () => (
+		<img src='/result-sources-icons/eggheadio.svg' alt='EggheadIo' />
+	),
+	color: '#141618',
+};
+const FreeCodeCamp = {
+	Image: () => (
+		<img src='/result-sources-icons/freecodecamp.jpg' alt='FreeCodeCamp' />
+	),
+	color: '0A0A23',
+};
+const Github = {
+	Image: () => <img src='/result-sources-icons/github.svg' alt='Github' />,
+	color: '#FAFBFC',
+};
+const Hashnode = {
+	Image: () => <img src='/result-sources-icons/hashnode.svg' alt='Hashnode' />,
+	color: '#2962FF',
+};
+const Medium = {
+	Image: () => <img src='/result-sources-icons/medium.png' alt='Medium' />,
+	color: 'linear-gradient(to right, #000000, #1A8917)',
+};
+const Quora = {
+	Image: () => <img src='/result-sources-icons/quora.png' alt='Quora' />,
+	color: '#AA2200',
+};
+const Reddit = {
+	Image: () => <img src='/result-sources-icons/reddit.svg' alt='Reddit' />,
+	color: '#FF5700',
+};
+const Sitepoint = {
+	Image: () => (
+		<img src='/result-sources-icons/sitepoint.jpg' alt='Sitepoint' />
+	),
+	color: 'FF6600',
+};
+const StackExchange = {
+	Image: () => (
+		<img src='/result-sources-icons/stackexchange.svg' alt='StackExchange' />
+	),
+	color: '#0074CC',
+};
+const StackOverflow = {
+	Image: () => (
+		<img src='/result-sources-icons/stackoverflow.svg' alt='StackOverflow' />
+	),
+	color: '#F48024',
+};
+const Youtube = {
+	Image: () => <img src='/result-sources-icons/youtube.svg' alt='Youtube' />,
+	color: '#FF0000',
+};
+
+const sourceInfo = {
+	CodeGrepper,
+	Codepen,
+	CodeProject,
+	CssTricks,
+	DevTo,
+	EggheadIo,
+	FreeCodeCamp,
+	Github,
+	Hashnode,
+	Medium,
+	Quora,
+	Reddit,
+	Sitepoint,
+	StackExchange,
+	StackOverflow,
+	Youtube,
+};
+
+export default sourceInfo;
